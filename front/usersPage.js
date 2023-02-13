4ebaa9a6b67cb58de5e59b70c8bfc8127bcdb6c0
Valiyeva Elmira <valiyeva1995elya@gmail.com>
2023-02-13 15:47:55 +0600

    done

--- front/usersPage.js
+++ front/usersPage.js
@@ -20,90 +20,88 @@ const fetchData = async (route) => {
 };
 
 
-// const postData = async (route, payload) => {
-//     fetch(
-//         BASE_URL + route,
-//         {
-//             method: 'POST',
-//             headers: {
-//                 'Accept': 'application/json',
-//                 'Content-Type': 'application/json'
-//             },
-//             data: payload
-//         },
-//     )
-//         .then(() => alert("OK"))
-//         .catch(() => alert("Error sending request"))
-// };
-usersContainer.innerHTML += ""
+const postData = async (route, payload) => {
+    fetch(
+        BASE_URL + route,
+        {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: payload
+        },
+    )
+    .then(() => console.log("parsed"))
+    .catch(() => console.log("Error sending request"));
+};
+
 const drawUsers = async () => {
-    usersContainer.innerHTML += ""
+    usersContainer.innerHTML = ""
+    let user = JSON.parse(localStorage.getItem('user'))
     const users = await fetchData("/accounts");
+    const userFollowed = user.follows
+    let usersAll = users.filter(item => item._id !==  user._id)
+    console.log(usersAll);
+    console.log(userFollowed);
+    console.log(user._id);
 
-
-
-    for (const user of users) {
-        usersContainer.innerHTML += `
+    usersAll.forEach(acc => {
+   
+        if(userFollowed.includes(acc._id)){
+            usersContainer.innerHTML += `
             <div class="user_item">
                 <tr>
-                    <td>${user.email}</td>
-                    <td>${user.name}</td>
-                    <td>${user.surname}</td>
-                    <td>${user.age}</td>
-                    <td> <button onclick="followAd('${user._id}')" class="btn-main fl">Follow</button><br>
-                    <button onclick="unFollowAuthor('${user._id}')" class="btn-main unfl">Unfollow</button></td>
+                    <td>${acc.email}</td>
+                    <td>${acc.name}</td>
+                    <td>${acc.surname}</td>
+                    <td>${acc.age}</td>
+                    <td><button onclick="unFollowAuthor('${acc._id}')" class="btn-main unfl">Unfollow</button></td>
                 </tr>
             </div>
-               
-        `
+            `
+        }else{
+            usersContainer.innerHTML += `
+            <div class="user_item">
+                <tr>
+                    <td>${acc.email}</td>
+                    <td>${acc.name}</td>
+                    <td>${acc.surname}</td>
+                    <td>${acc.age}</td>
+                    <td> <button onclick="followAd('${acc._id}')" class="btn-main fl">Follow</button><br></td>
+                </tr>
+            </div>
+            `
+        }
 
-    }
+        
+    })
 };
+const followAd = (followedUserId) => {
+    const userId = JSON.parse(localStorage.getItem("user"));
+    const payload = {
+        userId: userId._id,
+        followedUserId: followedUserId
+    }
+    const jsonPayload = JSON.stringify(payload);
+    postData("/accounts/follow", jsonPayload);
+    userId.follows.push(followedUserId)
+    localStorage.setItem("user", JSON.stringify(userId))
+    drawUsers();
+}
+
+const unFollowAuthor = (followedUserId) => {
+    const userId = JSON.parse(localStorage.getItem("user"));
+    
+    const payload = {
+        userId: userId._id,
+        followedUserId: followedUserId
+    }
+    const jsonPayload = JSON.stringify(payload);
+    postData("/accounts/unfollow", jsonPayload);
+    userId.follows = userId.follows.filter((id) => id != followedUserId)
+    console.log(userId);
+    localStorage.setItem("user", JSON.stringify(userId))
+    drawUsers();
+}
 drawUsers()
-
-async function followAd(userId) {
-    const user = JSON.parse(localStorage.getItem("user"))
-
-    const payload = {
-        "newData": `${user}`
-    }
-    
-    fetch(BASE_URL + "/accounts/follow/" + `${userId}`, {
-        method: "PUT",
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8'
-        },
-        body: JSON.stringify(payload)
-    })
-    .then(() => {
-        drawUsers()
-    })
-    .catch((err) => alert(err))
-}
-
-async function unFollowAuthor(userId) {
-    const user = JSON.parse(localStorage.getItem("user"))
-
-    const payload = {
-        "newData": `${user}`
-    }
-    
-    const ourUser = await fetchData("/accounts/" + `${userId}`)
-    if (ourUser.followers.indexOf(user)) {
-        fetch(BASE_URL + "/accounts/unfollow/" + `${userId}`, {
-            method: "PUT",
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8'
-            },
-            body: JSON.stringify(payload)
-        })
-        .then(() => {
-            drawUsers()
-        })
-        .catch((err) => alert(err))
-    } else {
-        alert("Вы не подписаны!")
-    }
-
-    
-}
